88880750176d0c27046292ca09f30bba733c1141
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2023-03-29 09:49:01 +0300

    module

---
+++ Module/Controllers/ModuleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Module.Controllers
+{
+    public class ModuleController : Controller
+    {
+        [HttpGet("task1")]
+        public ActionResult Cosine(int r)
+        {
+            if (r > 20)
+            {
+                return BadRequest("r повинно бути більше 20");
+            }
+            int R = 20;
+            double S = Math.PI*(R^2 - r^2);
+
+            return Ok(S);
+        }
+
+        [HttpGet("task2")]
+        public double CalculateCosSum(int n)
+        {
+            if (n < 1)
+            {
+                throw new ArgumentException("n не натуральне число");
+            }
+
+            int count = 0;
+
+            while (n != 0)
+            {
+                count++;
+                n /= 10;
+            }
+
+            return (count);
+        }
+    }
+}
